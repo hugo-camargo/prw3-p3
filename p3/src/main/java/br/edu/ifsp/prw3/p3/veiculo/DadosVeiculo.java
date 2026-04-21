7677bad8b789764784a0e7d3c15ca427f455a8c0
Luigi <a.luigi@aluno.ifsp.edu.br>
2026-04-21 18:09:48 -0300

    verificacoes de padrao de dados de veiculo

--- p3/src/main/java/br/edu/ifsp/prw3/p3/veiculo/DadosVeiculo.java
+++ p3/src/main/java/br/edu/ifsp/prw3/p3/veiculo/DadosVeiculo.java
@@ -1,3 +1,16 @@
 package br.edu.ifsp.prw3.p3.veiculo;
 
-public record DadosVeiculo(String marca, String modelo, String ano) {}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosVeiculo(
+
+        @NotBlank
+        String marca,
+
+        @NotBlank
+        String modelo,
+
+        @NotBlank
+        @Pattern(regexp = "^\\d{4}$", message = "O ano deve ter exatamente 4 dígitos")
+        String ano) {}
